966d066cb2e8042974471d3b0d4069ee43c09ecc
Nick Fitton <nick.fitton@ovo.com>
2023-02-17 13:49:17 +0000

    Setup RNMCSDK

---
+++ ios/SalesforceNSE/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  SalesforceNSE
+//
+//  Created by Nick Fitton on 17/02/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
