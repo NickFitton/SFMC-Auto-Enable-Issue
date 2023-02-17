443946573c56cc728bee8594c4d1312b6099fa77
Nick Fitton <nick.fitton@ovo.com>
2023-02-17 13:54:47 +0000

    Proof that push is enabled by default, something we would like a choice in

--- App.js
+++ App.js
@@ -6,52 +6,26 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Text,
 } from 'react-native';
+import MCReactModule from 'react-native-marketingcloudsdk';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
+  const [isPushEnabled, setIsPushEnabled] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    MCReactModule.isPushEnabled().then(setIsPushEnabled);
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -71,29 +45,16 @@ const App = () => {
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}></View>
+          }}>
+          {isPushEnabled ? (
+            <Text>Push is enabled</Text>
+          ) : (
+            <Text>Push is disabled</Text>
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
